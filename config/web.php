96bcc96f69cafdab2db9f7e81e09ab46029a831f
shurek <shurek@pop3.ru>
2015-09-04 21:36:17 +0500

    Изменен ключ куки на OMSdbPrint

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '6502255789',
+            'cookieValidationKey' => 'OMSdbPrint',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
