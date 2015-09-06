9b9fa7ffabcfa53337a7598c50d5a730bbad9756
shurek <shurek@pop3.ru>
2015-09-06 15:26:34 +0500

    Добавид параметр layout

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'layout' => 'index',
     'components' => [
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
