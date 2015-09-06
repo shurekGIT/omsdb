9550bc0fb1ea48a65523b761189e38b6d21a7a34
shurek <shurek@pop3.ru>
2015-09-06 16:33:26 +0500

    Новый шаблон сайта

--- config/web.php
+++ config/web.php
@@ -7,6 +7,11 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'layout' => 'index',
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+        ],
+    ],
     'components' => [
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
