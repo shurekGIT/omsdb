1b478c4ac9770c616f8d43f0493cd7033ad703a8
shurek <shurek@pop3.ru>
2015-09-06 15:25:30 +0500

    Прописаны пути до стилей и скриптов для нового шаблона

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -18,9 +18,21 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'theme/bootstrap/css/bootstrap.min.css',
+        'theme/download/font-awesome.min.css',
+        'theme/download/ionicons.min.css',
+        'theme/dist/css/AdminLTE.min.css',
+        'theme/dist/css/skins/_all-skins.min.css',
+      
+        
+        
+        
     ];
     public $js = [
+        'theme/bootstrap/js/bootstrap.min.js',
+        'theme/plugins/fastclick/fastclick.min.js',
+        'theme/dist/js/app.min.js',
+        'theme/dist/js/demo.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
